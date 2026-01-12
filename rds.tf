ba21d7ecd30f1baeca1348971e15af43607aaa39
Nandini99g <nandinigadambli@gmail.com>
2026-01-12 22:33:38 +0530

    Added screenshots

--- rds.tf
+++ rds.tf
@@ -12,4 +12,8 @@ resource "aws_db_instance" "mysql" {
   db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   publicly_accessible    = false
+
+  skip_final_snapshot  = true
+  deletion_protection = false
+  apply_immediately   = true
 }
